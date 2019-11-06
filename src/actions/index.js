edb711887201ad6da0ed40b71bfc9888e3f08ac5
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-11-06 10:05:06 -0700

    update dom with new order and it is persisting

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ export const setOrders = orders => ({
   type: 'SET_ORDERS',
   orders
 });
+
+export const sendNewOrder = order => ({
+  type: 'SEND_ORDER',
+  order
+});
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,19 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const submitOrder = (order) => {
+  const url = 'http://localhost:3001/api/v1/orders';
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(order),
+    headers: {
+        'Content-Type': 'application/json'
+    }
+  }
+  
+  return fetch(url, options)
+      .then(response => response.json())
+        // .then(order => this.setState({ orders : [...this.state.orders, order ]}));
+        // .catch(error => console.log(error));
+}

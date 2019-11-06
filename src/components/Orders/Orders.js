807f44ebbb74d34c971e32a732da49340a874b4d
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-11-06 09:33:19 -0700

    get all standing orders on page

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,25 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Orders.css';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { setOrders } from '../../actions';
+import { getOrders } from '../../apiCalls';
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
+export class Orders extends Component {
+  constructor() {
+    super();
+    this.state = {
+      orders: []
+    }
+  }
+
+  componentDidMount() {
+    const { setOrders } = this.props;
+    getOrders()
+      .then(data => setOrders(data.orders))
+      .catch(err => console.error('Error fetching:', err));
+  }
+
+   orderEls = () => {
+    const { orders } = this.props;
+    return orders.map(order => {
+      return (
+        <div className="order" key={order.id}>
+          <h3>{order.name}</h3>
+          <ul className="ingredient-list">
+            {order.ingredients.map(ingredient => {
+              return <li>{ingredient}</li>
+            })}
+          </ul>
+        </div>
+      )
+    })
+  };
+  
+  render () {
     return (
-      <div className="order">
-        <h3>{order.name}</h3>
-        <ul className="ingredient-list">
-          {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
-          })}
-        </ul>
-      </div>
+      <section>
+        {  this.props.orders.length ? this.orderEls() : <p>No orders yet!</p> }
+      </section>
     )
-  });
-
-  return (
-    <section>
-      { orderEls.length ? orderEls : <p>No orders yet!</p> }
-    </section>
-  )
+  }
+  
 }
 
-export default Orders;
+export const mapStateToProps = ({ orders }) => ({
+  orders
+});
+
+export const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    setOrders,
+  }, dispatch)
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Orders);

05a1159440d1b78261768e9d8999251bd95041dd
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-11-06 11:33:52 -0700

    update mapDispatchToProps test

---
+++ src/components/Orders/Orders.test.js
@@ -0,0 +1,50 @@
+import { Orders, mapDispatchToProps, mapStateToProps } from './Orders';
+import { shallow } from 'enzyme';
+import React from 'react';
+import { setOrders } from '../../actions/index';
+
+describe('Orders', () => {
+    let wrapper;
+    const mockOrders = [{
+        name: 'Ayla',
+        ingredients:['pineapple', 'cucumber', 'mayo']
+    },{
+      name: 'tina',
+      ingredients:['poodles', 'cattle', 'mayo']
+  }]
+
+    beforeEach(() => {
+        wrapper = shallow(<Orders orders={mockOrders}/>)
+    });
+
+    it('should match the snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+})
+
+describe('MapDispatchToProps', () => {
+    let mockDispatchedAction;
+    
+    beforeEach(() => {
+      mockDispatchedAction = jest.fn();
+    })
+
+    it('should call call setOrders action with dispatch', () => {
+      const mockOrders = [{
+          name: 'Ayla',
+          ingredients:['pineapple', 'cucumber', 'mayo']
+      },{
+        name: 'tina',
+        ingredients:['poodles', 'cattle', 'mayo']
+    }
+    
+    ]
+      const mockAction = setOrders(mockOrders);
+      const props = mapDispatchToProps(mockDispatchedAction);
+  
+      props.setOrders(mockOrders);
+  
+      expect(mockDispatchedAction).toHaveBeenCalledWith(mockAction);
+    });
+
+    });
